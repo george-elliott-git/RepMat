65afdca5708fdeb1f3578672756df707138778ea
George Elliott <georgeelliott.ge@proton.me>
2025-05-09 23:08:30 +0100

    Update test_representations.py

--- tests/test_representations.py
+++ tests/test_representations.py
@@ -1,6 +1,6 @@
 # test_representations.py
 
-# Here we test: CUSTOM REPRESENTATIONS, REP. HOMOMORPHISMS, REP. IRREDUCIBILITY/DECOMPOSABILITY
+# Here we test: CUSTOM REPRESENTATIONS, REP. HOMOMORPHISMS, REP. IRREDUCIBILITY, WE ALSO TEST ALL THE FUNCTIONALITY IN CHARCTERS, HENCE THE ABSENCE OF THE TEST_CHARACTERS.PY
 
 import unittest
 import numpy as np
@@ -8,6 +8,7 @@ import numpy as np
 import RepMat
 from RepMat import groups, representations, vspaces
 from RepMat import groups, create_Group, Cyclic, Sym, Alt, Dihe, Optn
+from RepMat import characters, Character, Char_Table
 
 class test_custom_groups(unittest.TestCase):
     def setUp(self):
@@ -18,7 +19,12 @@ class test_custom_groups(unittest.TestCase):
         custom_group = groups.Group(self.test_custom_set, self.test_custom_operation)
         test_matrices = [np.array([[1, 0], [0, 1]]), np.array([[0, -1], [1, 0]]), np.array([[-1, 0], [0, -1]]), np.array([[0, 1], [-1, 0]])]  # 270-degree rotation matrix
         vspace = vspaces.R(1)
+        global custom_rep
         custom_rep = representations.Rep(custom_group, test_matrices, vspace)
+        
+    def test_character_table(self):
+        characters.Char(custom_rep)
+        Char_Table(custom_rep)
         
 if __name__ == '__main__':
     unittest.main()
